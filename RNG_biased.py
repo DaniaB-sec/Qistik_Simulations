14db7bf67724249b3469be57e55c1ff3e2acba4b
Danya <daniabeck.sec@gmail.com>
2026-09-09 08:59:07 -0300

    Add files via upload

---
+++ RNG_biased.py
@@ -0,0 +1,57 @@
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import Aer
+import math
+
+
+BIAS = 0.80
+
+theta = 2 * math.asin(math.sqrt(BIAS))
+
+qc = QuantumCircuit(10, 10)
+
+
+for q in range(10):
+
+   
+    qc.h(q)
+
+  
+    qc.s(q)
+    qc.t(q)
+
+  
+    qc.rz(theta - 3 * math.pi / 4, q)
+
+    
+    qc.h(q)
+
+
+qc.measure(range(10), range(10))
+
+shots = 1000
+
+backend = Aer.get_backend("qasm_simulator")
+
+compiled_circuit = transpile(qc, backend)
+
+run_out = backend.run(
+    compiled_circuit,
+    shots=shots
+).result()
+
+out_counts = run_out.get_counts()
+
+print("Raw quantum results:")
+print(out_counts)
+
+valid_counts = {}
+
+for bitstring, count in out_counts.items():
+    number = int(bitstring, 2)
+
+    if number <= 1000:
+        valid_counts[number] = count
+
+
+print("\nBiased RNG results (0-1000):")
+print(valid_counts)
